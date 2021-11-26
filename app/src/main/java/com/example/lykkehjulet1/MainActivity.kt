083cf77391ed6722c91a077245d2a38798a215ac
Kageschwump <kasperfalch@live.dk>
2021-11-26 23:17:24 +0100

    Made the start screen and a fragment for the 'game' portion of the app

--- app/src/main/java/com/example/lykkehjulet1/MainActivity.kt
+++ app/src/main/java/com/example/lykkehjulet1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.lykkehjulet1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
     }
 }
